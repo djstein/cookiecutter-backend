7612239ac47a44eb62037d4ad7a158e13f2dadae
Dylan Stein <djstein@ncsu.edu>
2018-07-27 00:37:30 -0400

    ensure static url is base

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -151,7 +151,7 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
 STATIC_ROOT = str(ROOT_DIR('static'))
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = 'static/'
+STATIC_URL = '{{cookiecutter.stage}}/static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
     str(APPS_DIR.path('static')),
